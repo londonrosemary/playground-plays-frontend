39339eb1b6c47749a7d280939706d60b5cd01e35
London <london.rosemary@gmail.com>
2023-01-03 22:12:40 -0500

    React Router implemented and working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
 import './App.css';
 
 import NavBar from './Components/NavBar';
@@ -7,10 +14,19 @@ import Plays from './Components/Plays/Plays'
 
 function App() {
   return (
-        <div>
-          <NavBar/>
-          <Plays/>
-      </div>  
+    <div>
+      {/* Anything that will live on all pages will need to go outside of the routes */}
+      <NavBar />
+      {/* Need the Routes indicator inside main App to allow all routes to work */}
+      <Routes>
+        {/* Route requires a path and an element.
+              Path is for the URL
+              Element is for the component being rendered */}
+          <Route exact path='/plays' element={<Plays />}/>
+      </Routes> 
+    </div>
+      
+
   );
 }
 
